55ecc9127ed135aad835b0ea3aa575f1ae11b2f6
Alexandr9233 <shurik923923@gmail.com>
2020-04-22 21:58:49 +0300

    Second task executed

---
+++ Task2/script.js
@@ -0,0 +1,39 @@
+// let a = 30;
+
+// //(a < 39 && a == 32) ? console.log('Yes!'): console.log('No');
+
+// switch (true) {
+//   case a > 33:
+//     console.log("No!");
+//     break;
+//   case a < 51:
+//     console.log("Also No!");
+//     break;
+//   case a == 30:
+//     console.log("Yes!");
+//     break;
+// }
+
+// for (let i = test; i < 8; i++) {
+//   if (i == 6) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+// let test = 0;
+// do {
+//   test++;
+//   if (test == 6) {
+//     continue;
+//   }
+//   console.log(test);
+// } while (test < 8);
+
+while (test < 8) {
+  test++;
+  if (test == 6) {
+    continue;
+  }
+
+  console.log(test);
+}
